7496bd581f7159eaaf48ef637a86c21407036e11
Lasitha <dakumpitiyalasitha2@gmail.com>
2022-12-21 06:30:36 +0530

    Add project files.

---
+++ RunGroup/Controllers/RacesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroup.Data;
+using RunGroup.Models;
+
+namespace RunGroup.Controllers
+{
+    public class RacesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public RacesController(ApplicationDbContext context)
+        {
+            _context= context;
+        }
+        public IActionResult Index()
+        {
+            List<Races> races = _context.Races.ToList();
+            return View(races);
+        }
+    }
+}
---
+++ RunGroup/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroup.Models;
+
+namespace RunGroup.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Races> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
