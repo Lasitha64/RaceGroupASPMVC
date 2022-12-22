781ea85a68aaf93872ee05e9813776885c58bcb2
Lasitha <dakumpitiyalasitha2@gmail.com>
2022-12-22 20:35:13 +0530

    Login feature

---
+++ RunGroup/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroup.Data;
+using RunGroup.Models;
+using RunGroup.ViewModels;
+
+namespace RunGroup.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+            if(!ModelState.IsValid) return View(login);
+
+            var user = await _userManager.FindByEmailAsync(login.EmailAddress);
+
+            if(user != null)
+            {
+                //user is found
+                var passwordCheck = await _userManager.CheckPasswordAsync(user,login.Password);
+                if (passwordCheck)
+                {
+                    //correct password sign in
+                    var result = await _signInManager.PasswordSignInAsync(user, login.Password, false, false);
+                    if(result.Succeeded)
+                    {
+                        return RedirectToAction("Index","Races");
+                    }
+                }
+                //password is incorrect
+                TempData["Error"] = "Wrong credentials. Please Try again";
+                return View(login);
+            }
+            //user not found
+            TempData["Error"] = "Wrong credentials. Please Try again";
+            return View(login);
+        }
+    }
+}
